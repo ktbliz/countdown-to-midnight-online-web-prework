fb6267c738dc1f20467287469524ce8735ba5438
Katie Blizzard <kblizzard11@gmail.com>
2019-02-02 18:32:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,7 @@ def countdown(seconds)
     puts "#{seconds} SECOND(S)!"
     seconds -= 1 
   end
-  puts "HAPPY NEW YEAR!"
+  puts "HAPPY NEW YEAR!" if seconds == 0
   return "HAPPY NEW YEAR!"
 end
 
